f3824dd4d356bc862e56f55c3a88271648bcc14f
linker85 <linker85@hotmail.com>
2016-11-18 23:44:58 -0500

    Pojo injection

--- app/src/main/java/samples/com/roboguicebysteps/MainActivity.java
+++ app/src/main/java/samples/com/roboguicebysteps/MainActivity.java
@@ -3,6 +3,8 @@ package samples.com.roboguicebysteps;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.inject.Inject;
+
 import roboguice.activity.RoboActionBarActivity;
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
@@ -13,10 +15,15 @@ public class MainActivity extends RoboActionBarActivity {
     @InjectView(R.id.atext_id)
     public TextView atextId;
 
+    @Inject
+    public User user; // this will basically call new Foo();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        atextId.setText("Injected with Roboguice");
+        user.setName("Injected with Roboguice");
+
+        atextId.setText(user.getName());
     }
 }
---
+++ app/src/main/java/samples/com/roboguicebysteps/User.java
@@ -0,0 +1,24 @@
+package samples.com.roboguicebysteps;
+
+
+/**
+ * Created by raul on 18/11/2016.
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
