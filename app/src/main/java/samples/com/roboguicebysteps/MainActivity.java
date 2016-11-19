1c7e7d61417d94952b8ed1d612ba9d0e9d42bfcd
linker85 <linker85@hotmail.com>
2016-11-18 23:33:13 -0500

    Simple injection of component

--- app/src/main/java/samples/com/roboguicebysteps/MainActivity.java
+++ app/src/main/java/samples/com/roboguicebysteps/MainActivity.java
@@ -1,13 +1,21 @@
 package samples.com.roboguicebysteps;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import roboguice.activity.RoboActionBarActivity;
+import roboguice.inject.InjectView;
+
+public class MainActivity extends RoboActionBarActivity {
+
+    @InjectView(R.id.atext_id)
+    public TextView atextId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        atextId.setText("Injected with Roboguice");
     }
 }
